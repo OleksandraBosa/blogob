9a78e1192b21082de7ce43a5cd52508922cbebcd
obosa <shuraka665@gmail.com>
2018-05-19 20:02:18 +0300

    first commit

---
+++ Models/Post.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ResumeBlog.Models
+{
+    public class Post
+    {
+		public int PostId { get; set; }
+		public string Title { get; set; }
+		public string Content { get; set; }
+
+		public string Image { get; set; }
+
+		public int BlogId { get; set; }
+		public Blog Blog { get; set; }
+	}
+}
